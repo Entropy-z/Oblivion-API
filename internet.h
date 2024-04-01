5af263f5094b7ce775db82d3e895b2cc905e6a3a
Oblivion 悪 <144470660+Entropy-z@users.noreply.github.com>
2024-04-01 18:17:06 -0300

    Update internet.h

--- internet.h
+++ internet.h
@@ -88,4 +88,114 @@ _EndOfFunction:
 }
 */
 
-BOOL Winhttp();
+/*
+BOOL Winhttp(wchar_t* whost, DWORD port, wchar_t* wresource) {
+    struct DLL dll;
+    std::vector<unsigned char> PEbuf;
+    DWORD dwSize = 0;
+    DWORD dwDownloaded = 0;
+    LPSTR pszOutBuffer = NULL;
+    BOOL  bResults = FALSE;
+    HINTERNET  hSession = NULL,
+        hConnect = NULL,
+        hRequest = NULL;
+    // Use WinHttpOpen to obtain a session handle.
+    hSession = WinHttpOpen(L"WinHTTP Example/1.0",
+        WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
+        WINHTTP_NO_PROXY_NAME,
+        WINHTTP_NO_PROXY_BYPASS, 0);
+
+
+    // Specify an HTTP server.
+    if (hSession)
+        hConnect = WinHttpConnect(hSession, whost,
+            port, 0);
+    else
+        printf("Failed in WinHttpConnect (%u)\n", GetLastError());
+
+    // Create an HTTP request handle.
+    if (hConnect)
+        hRequest = WinHttpOpenRequest(hConnect, L"GET", wresource,
+            NULL, WINHTTP_NO_REFERER,
+            WINHTTP_DEFAULT_ACCEPT_TYPES,
+            NULL);
+    else
+        printf("Failed in WinHttpOpenRequest (%u)\n", GetLastError());
+
+    // Send a request.
+    if (hRequest)
+        bResults = WinHttpSendRequest(hRequest,
+            WINHTTP_NO_ADDITIONAL_HEADERS,
+            0, WINHTTP_NO_REQUEST_DATA, 0,
+            0, 0);
+    else
+        printf("Failed in WinHttpSendRequest (%u)\n", GetLastError());
+
+    // End the request.
+    if (bResults)
+        bResults = WinHttpReceiveResponse(hRequest, NULL);
+    else printf("Failed in WinHttpReceiveResponse (%u)\n", GetLastError());
+
+    // Keep checking for data until there is nothing left.
+    if (bResults)
+        do
+        {
+            // Check for available data.
+            dwSize = 0;
+            if (!WinHttpQueryDataAvailable(hRequest, &dwSize))
+                printf("Error %u in WinHttpQueryDataAvailable (%u)\n", GetLastError());
+
+            // Allocate space for the buffer.
+            pszOutBuffer = new char[dwSize + 1];
+            if (!pszOutBuffer)
+            {
+                printf("Out of memory\n");
+                dwSize = 0;
+            }
+            else
+            {
+                // Read the Data.
+                ZeroMemory(pszOutBuffer, dwSize + 1);
+
+                if (!WinHttpReadData(hRequest, (LPVOID)pszOutBuffer,
+                    dwSize, &dwDownloaded))
+                    printf("Error %u in WinHttpReadData.\n", GetLastError());
+                else {
+
+
+                    PEbuf.insert(PEbuf.end(), pszOutBuffer, pszOutBuffer + dwDownloaded);
+                     
+                    
+
+                }
+                delete[] pszOutBuffer;
+
+            }
+
+        } while (dwSize > 0);
+
+        if (PEbuf.empty() == TRUE)
+        {
+            printf("Failed in retrieving the PE");
+        }
+
+        // Report any errors.
+        if (!bResults)
+            printf("Error %d has occurred.\n", GetLastError());
+
+        // Close any open handles.
+        if (hRequest) WinHttpCloseHandle(hRequest);
+        if (hConnect) WinHttpCloseHandle(hConnect);
+        if (hSession) WinHttpCloseHandle(hSession);
+
+        size_t size = PEbuf.size();
+        //printf("size : %d\n", size);
+        char* ntdll = (char*)malloc(size);
+        for (int i = 0; i < PEbuf.size(); i++) {
+            ntdll[i] = PEbuf[i];
+        }
+        dll.ntdll = ntdll;
+        dll.size = size;
+        return dll;
+}
+*/
