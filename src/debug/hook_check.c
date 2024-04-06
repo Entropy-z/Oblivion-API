cf42ba589b659dd7136b45193e8b86eb580cb2b0
Josué Magalhães <entropymalws@gmail.com>
2024-04-06 08:18:16 -0300

    added debug and hook check

---
+++ src/debug/hook_check.c
@@ -0,0 +1,6 @@
+#include <Windows.h>
+#include <debug/debug.h>
+
+VOID HookCheck(char* SyscallName, PVOID pSyscallAddress) {
+	printf("[#] %s [ 0x%p ] ---> %s \n", SyscallName, pSyscallAddress, (*(ULONG*)pSyscallAddress != 0xb8d18b4c) == TRUE ? "[ HOOKED ]" : "[ UNHOOKED ]");
+}
