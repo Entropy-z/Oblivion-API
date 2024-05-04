487f0fe6c1711d1560993010baace17188914bde
Oblivion 悪 <144470660+Entropy-z@users.noreply.github.com>
2024-05-04 17:26:45 -0300

    Update mem_manipulation.c

--- src/core/mem_manipulation.c
+++ src/core/mem_manipulation.c
@@ -25,13 +25,7 @@ VOID ZeroMemoryEx(_Inout_ PVOID Destination, _In_ SIZE_T Size){
 	return;
 }
 
-/*--------------------------------------------------------------------------------------*/
-
-#pragma intrinsic(memset)
-
-#pragma function(memset)
-
-void* __cdecl memset(void* Destination, int Value, size_t Size){
+PVOID MemSet(void* Destination, int Value, size_t Size){
 	unsigned char* p = (unsigned char*)Destination;
 	while (Size > 0) {
 		*p = (unsigned char)Value;
